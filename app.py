a20e6a68585aa4cc5eaf67fd590f9ba4288e56bb
xProSergi <sergioinstituto21.22@gmail.com>
2025-12-01 18:13:58 +0100

    Parklytics final

--- app.py
+++ app.py
@@ -271,7 +271,6 @@ def render_hero():
                     right: 0;
                     bottom: 0;
                     left: 0;
-                    background-color: rgba(0, 0, 0, 0.4); /* Capa negra semi-transparente */
                     z-index: 0;
                 }}
 
